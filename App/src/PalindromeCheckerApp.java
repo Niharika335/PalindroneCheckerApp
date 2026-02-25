0f7bffebf9488a0600f0e3a02c25858dd0150884
Niharika335 <niharika26335@gmail.com>
2026-02-25 09:37:14 +0530

    Niharika added UC-2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,22 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome checker app Management sysyem");
-    }
+    public static void main(String[] args) {
+        // Hardcoded string
+        String word = "madam";
 
+        boolean isPalindrome = true;
+        int n = word.length();
+
+        // Loop only until the halfway point
+        for (int i = 0; i < n / 2; i++) {
+            // Compare character at start with character at symmetric end
+            if (word.charAt(i) != word.charAt(n - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Displaying the result
+        System.out.println("String: " + word);
+        System.out.println("Is a palindrome?: " + isPalindrome);
+    }
 }
